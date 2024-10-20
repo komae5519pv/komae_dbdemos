56e2a9c99676c3edd51c35596101adbcd6e4598d
komae5519pv <komae5519pv>
2024-10-20 08:29:03 +0000

    miner fix

--- automl_demo_20241011/02_ETL_Pipeline.sql
+++ automl_demo_20241011/02_ETL_Pipeline.sql
@@ -82,13 +82,15 @@
 -- MAGIC ### 1. パイプライン手動設定
 -- MAGIC パイプライン > パイプライン設定
 -- MAGIC 一般
+-- MAGIC 下記内容で設定してください
 -- MAGIC ```
--- MAGIC パイプライン名: komae_automl_e2e_demo
+-- MAGIC パイプライン名: komae_automl_e2e_demo_dlt
 -- MAGIC パイプラインモード: トリガー（バッチ処理）
 -- MAGIC ```
 -- MAGIC
 -- MAGIC
--- MAGIC Advanced
+-- MAGIC パイプラインを作成 > Advanced > 設定を追加
+-- MAGIC キーと値を設定してください
 -- MAGIC ```
 -- MAGIC init_setting.catalog: <カタログ名を指定してください>
 -- MAGIC init_setting.schema : automl_e2e_demo
