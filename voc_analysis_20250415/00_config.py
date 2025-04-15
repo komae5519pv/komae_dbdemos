e084b1b2170b58cf2da77165db47ae91539967f0
komae5519pv <komae5519pv>
2025-04-15 15:04:20 +0000

    1st commit

---
+++ voc_analysis_20250415/00_config.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# カタログ、スキーマ、ボリューム
+MY_CATALOG = "komae_demo_v3"             # 使用したいカタログ名に変更してください
+MY_SCHEMA = "airline_reviews"
+MY_VOLUME = "raw_data"
+MY_VOLUME_IMAGE = "images"
+
+# ボリュームディレクトリ
+VOLUME_EN = f"/Volumes/{MY_CATALOG}/{MY_SCHEMA}/{MY_VOLUME}/EN"
+VOLUME_JA = f"/Volumes/{MY_CATALOG}/{MY_SCHEMA}/{MY_VOLUME}/JA"
+
+# COMMAND ----------
+
+# カタログ、スキーマ、ボリューム作成
+# spark.sql(f"CREATE CATALOG IF NOT EXISTS {MY_CATALOG}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {MY_CATALOG}.{MY_SCHEMA}")
+spark.sql(f"CREATE VOLUME IF NOT EXISTS {MY_CATALOG}.{MY_SCHEMA}.{MY_VOLUME}")
+spark.sql(f"CREATE VOLUME IF NOT EXISTS {MY_CATALOG}.{MY_SCHEMA}.{MY_VOLUME_IMAGE}")
+
+# ボリュームのサブディレクトリ作成
+dbutils.fs.mkdirs(VOLUME_EN)
+dbutils.fs.mkdirs(VOLUME_JA)
+
+spark.sql(f"USE CATALOG {MY_CATALOG}")
+spark.sql(f"USE SCHEMA {MY_SCHEMA}")
+
+print(f"MY_CATALOG: {MY_CATALOG}")
+print(f"MY_SCHEMA: {MY_SCHEMA}")
+print(f"MY_VOLUME: {MY_VOLUME}")
+print(f"MY_VOLUME_IMAGE: {MY_VOLUME_IMAGE}")
+print(f"ボリュームパス")
+print(VOLUME_EN)
+print(VOLUME_JA)
---
+++ voc_analysis_20250415/01_load_data.py
@@ -0,0 +1,47 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Githubのデータをダウンロード
+# MAGIC - ここではデモ用のサンプルデータをGithubからボリュームにロードします  
+# MAGIC - サーバレス or DBR 16.0ML以降
+
+# COMMAND ----------
+
+# MAGIC %run ./00_config
+
+# COMMAND ----------
+
+import requests
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+class DBDemos:
+    @staticmethod
+    def download_file_from_git(dest, owner, repo, path):
+        def download_file(url, destination):
+            local_filename = url.split('/')[-1]
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                print(f'Saving {destination}/{local_filename}')
+                with open(f'{destination}/{local_filename}', 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
+        api_url = f'https://api.github.com/repos/{owner}/{repo}/contents{path}'
+        files = requests.get(api_url).json()
+        download_urls = [f['download_url'] for f in files if isinstance(f, dict) and 'download_url' in f]
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(lambda url: download_file(url, dest), download_urls)
+
+# COMMAND ----------
+
+# ポジネガアイコンをダウンロード
+DBDemos.download_file_from_git(
+    dest=f"/Volumes/{MY_CATALOG}/{MY_SCHEMA}/{MY_VOLUME_IMAGE}/images",
+    owner="komae5519pv",
+    repo="komae_dbdemos",
+    path="/voc_analysis_20250415/_images/"
+)
